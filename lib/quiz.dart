382d49cee4499209b2c0228547f402608b884f22
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2023-06-22 00:47:55 +0530

    App finalizations

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/start_screen.dart';
-import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/results_screen.dart';
+import 'package:quiz_app/result_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
